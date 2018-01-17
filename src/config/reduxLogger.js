4cd194b41c62616a6dccfbf2936cc21265fb10e0
Quentin Brosse <quentin.brosse@epitech.eu>
2018-01-17 22:33:14 +0100

    Install redux-devtools-extension and remove redux-logger

--- src/config/reduxLogger.js
+++
@@ -1,9 +0,0 @@
-import { inDevEnv } from 'utils/env';
-
-const isDebuggingInChrome = inDevEnv() && !!window.navigator.userAgent;
-
-export default {
-  predicate: () => isDebuggingInChrome,
-  collapsed: true,
-  duration: true,
-};
--- src/utils/createStore.js
+++ src/utils/createStore.js
@@ -1,21 +1,18 @@
-import { compose, createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import firebase from 'firebase';
 import 'firebase/firestore';
 import { getFirebase, reactReduxFirebase } from 'react-redux-firebase';
 import { reduxFirestore } from 'redux-firestore';
-import { createLogger } from 'redux-logger';
 import { createEpicMiddleware } from 'redux-observable';
 import makeRootReducer from 'reducers';
 import makeRootEpic from 'epics';
 import { credential, config } from 'config/firebase';
-import reduxLoggerConfig from 'config/reduxLogger';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 
 export default initialState => {
   firebase.initializeApp(credential);
 
   firebase.firestore();
-
-  const loggerMiddleware = createLogger(reduxLoggerConfig);
 
   const epicMiddleware = createEpicMiddleware(makeRootEpic(), {
     dependencies: { getFirebase },
@@ -24,10 +21,9 @@ export default initialState => {
   const store = createStore(
     makeRootReducer(),
     initialState,
-    compose(
+    composeWithDevTools(
       reactReduxFirebase(firebase, config),
       reduxFirestore(firebase),
-      applyMiddleware(loggerMiddleware),
       applyMiddleware(epicMiddleware)
     )
   );
