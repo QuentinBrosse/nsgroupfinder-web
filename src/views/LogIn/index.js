863d7c9bff0901055f2d402208f356f2a9f1bdfb
Sofiane Taleb <sofiane.taleb.94@gmail.com>
2018-02-22 17:37:27 +0100

    First implementation of landing page

--- src/views/LogIn/index.js
+++ src/views/LogIn/index.js
@@ -6,9 +6,11 @@ import { connect } from 'react-redux';
 import { withFirebase } from 'react-redux-firebase';
 import { Redirect } from 'react-router-dom';
 import { withStyles } from 'material-ui/styles';
-import Typography from 'material-ui/Typography';
+import red from 'material-ui/colors/red';
+import indigo from 'material-ui/colors/indigo';
 import Button from 'material-ui/Button';
 import Paper from 'material-ui/Paper';
+import Stepper, { Step, StepButton } from 'material-ui/Stepper';
 import { throwDissmissSnackbar, throwAccentSnackbar } from 'actions/snackbar';
 import { isConnected } from 'utils/user';
 import { logErrorIfDevEnv } from 'utils/env';
@@ -76,26 +78,41 @@ class LogIn extends React.Component<Props, State> {
     }
     return (
       <div className={classes.container}>
-        <Paper className={classes.paper}>
-          <Typography variant="headline" align="center">
-            NS Group Finder
-          </Typography>
-          <Typography variant="subheading" align="center" color="primary">
-            Connect using Facebook{' '}
-            <span role="img" aria-label="Sad emojy">
-              😘
-            </span>
-          </Typography>
-
+        <div className={classes.beta}>
+          Please remember that this product is in beta. If find anything wrong
+          or want to give a feedback, do no hesitate to write on Twitter or
+          Messenger.
+        </div>
+        <div className={classes.top}>
+          <img className={classes.img} src="https://i.imgur.com/pbCT0Uh.jpg" />
+        </div>
+        <div className={classes.center}>
+          <div className={classes.step}>
+            <i className={'material-icons ' + classes.stepIcon}>search</i>
+            Search for your travel
+          </div>
+          <i class="material-icons">arrow_forward</i>
+          <div className={classes.step}>
+            <i className={'material-icons ' + classes.stepIcon}>group_work</i>
+            Connect with people
+          </div>
+          <i class="material-icons">arrow_forward</i>
+          <div className={classes.step}>
+            <i className={'material-icons ' + classes.stepIcon}>train</i>
+            Get your tickets & travel
+          </div>
+        </div>
+        <div className={classes.bottom}>
           <Button
             variant="raised"
             className={classes.button}
-            color="primary"
+            color="secondary"
             onClick={this.logIn}
           >
-            Login with Facebook
+            FIND A GROUP
+            <i class="material-icons">arrow_forward</i>
           </Button>
-        </Paper>
+        </div>
       </div>
     );
   }
@@ -104,15 +121,47 @@ class LogIn extends React.Component<Props, State> {
 const styles = ({ spacing }) => ({
   container: {
     display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'stretch',
+    justifyContent: 'flex-start',
     height: '100vh',
   },
-  paper: {
-    padding: [[spacing.unit * 8, spacing.unit * 6]],
+  beta: {
+    height: '5vh',
+    color: 'white',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: red['A700'],
   },
-  button: {
-    marginTop: spacing.unit * 6,
+  img: {
+    width: '100%',
+  },
+  top: {},
+  center: {
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    height: '30vh',
+  },
+  step: {
+    fontSize: 30,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+  },
+  stepIcon: {
+    fontSize: 100,
+    color: indigo['500'],
+    marginBottom: '3vh',
+  },
+  bottom: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
